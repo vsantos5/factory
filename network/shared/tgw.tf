e62acb5e978b75c54d1d9f7003729e0125a2999a
vsantos5 <vinicius.moura@outlook.com>
2025-05-06 13:43:49 -0300

    Network commit

---
+++ network/shared/tgw.tf
@@ -0,0 +1,13 @@
+module "tgw" {
+  for_each = { for tgws in local.workspace.tgw : tgws.name => tgws }
+  source   = "/home/vinicius/bazk/aws-infra/v2/tfmodules/tgw"
+
+  name                                   = try(each.value.name, "")
+  tgw_name                               = try(each.value.name, "")
+  amazon_side_asn                        = try(each.value.asn, "")
+  enable_share_tgw                       = true
+  enable_share_tgw_with_all_organization = true
+  enable_auto_accept_shared_attachments  = false
+
+  tags = local.default_tags
+}
---
+++ network/shared/vpc.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  for_each = { for vpcs in local.workspace.vpc : vpcs.name => vpcs }
+  source   = "/home/vinicius/bazk/aws-infra/v2/tfmodules/vpc"
+
+  vpc_cidr         = each.value.cidr_block
+  name             = try(each.value.name, "")
+  vpc_name         = try(each.value.vpc_name, "")
+  enable_flow_logs = true
+  public_subnets   = try(each.value.public_subnets, null)
+  private_subnets  = try(each.value.private_subnets, null)
+
+  enable_vpn_gateway = true
+  amazon_side_asn    = each.value.customer_gateway[0].bgp_asn
+  customer_gateways = {
+    VPN1 = {
+      bgp_asn    = each.value.customer_gateway[0].bgp_asn
+      ip_address = each.value.customer_gateway[0].ip_address
+    }
+  }
+
+  tags = local.default_tags
+}
